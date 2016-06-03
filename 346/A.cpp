03be4b695387a2350dd4b9b5afcb3b59ce1dcd23
wushangyangguang <993793072@qq.com>
2016-06-03 12:55:09 -0400

    add 355

---
+++ 346/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstdio>
+#include <stdlib.h>
+#include <cstring>
+#include <math.h>
+#include <set>
+#define PII pair<int, int>
+#define PFF pair<float, float>
+#define PDD pair<double, double>
+#define MP make_pair
+#define RI(X) scanf("%d", &(X))
+#define RII(X,Y) scanf("%d %d", &(X), &(Y))
+#define RIII(X,Y,Z) scanf("%d %d %d", &(X), &(Y), &(Z))
+#define RD(X) scanf("%lf", &(X))
+#define RDD(X,Y) scanf("%lf %lf", &(X), &(Y))
+#define RF(X) scanf("%f", &(X))
+#define RFF(X,Y) scanf("%f %f", &(X), &(Y))
+#define RL(X) scanf("%I64d", &(X))
+#define RLL(X,Y) scanf("%I64d %I64d", &(X), &(Y))
+#define LL long long
+#define MIN(X,Y) (X)<(Y)?(X):(Y)
+#define INFD 1e32
+#define PI 3.14159265358979323
+using namespace std;
+int main(){
+    int n,a,b;
+    RIII(n,a,b);
+    int res = (((a + b) % n) + n) % n;
+    cout << (res == 0 ? n : res) << endl;
+    return 0;
+}
